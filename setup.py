c034aca7139bfcf1e83089b6c3fe2b62e959627b
Maksim Rakitin <mrakitin@bnl.gov>
2020-06-18 22:01:36 -0400

    SDPX license

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setup(
         ]
     },
     install_requires=requirements,
-    license="BSD (3-clause)",
+    license="BSD-3-Clause",
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Natural Language :: English',
