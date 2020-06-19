5837a50d356349a161cc0b46137e53be44c604fd
Maksim Rakitin <mrakitin@bnl.gov>
2020-06-18 22:19:58 -0400

    Add console script

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
     packages=find_packages(exclude=['docs', 'tests']),
     entry_points={
         'console_scripts': [
-            # 'command = some.module:some_function',
+            'check-results = nsls2forge_utils.cli.main:main',
         ],
     },
     include_package_data=True,
