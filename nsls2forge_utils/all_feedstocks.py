a1b2d0da41b498e72d6d80d82f3d0cb4f307346e
Thomas Hopkins <thomashopkins000@gmail.com>
2020-06-23 10:05:58 -0400

    Resolved requested documentation changes

--- nsls2forge_utils/all_feedstocks.py
+++ nsls2forge_utils/all_feedstocks.py
@@ -1,3 +1,8 @@
+'''
+This code is a rework of
+https://github.com/regro/cf-scripts/blob/master/conda_forge_tick/all_feedstocks.py
+for use by nsls-ii-forge's own auto-tick bot.
+'''
 import requests
 import github3
 import logging
@@ -7,17 +12,17 @@ logger = logging.getLogger(__name__)
 
 def get_all_feedstocks_from_github(organization):
     '''
-    Gets all public feedstock repository names from the nsls-ii-forge organization.
-
+    Gets all public feedstock repository names from the GitHub organization
+    (e.g. nsls-ii-forge).
 
     Parameters
     ----------
-    organization : str
+    organization: str
         Name of organization on GitHub.
 
     Returns
     -------
-    names : List
+    names: list
         List of repository names that end with '-feedstock' (stripped).
     '''
     org = github3.organization(organization)
@@ -46,22 +51,25 @@ def get_all_feedstocks(organization, cached=False):
 
     Parameters
     ----------
-    organization : str
+    organization: str
         Name of organization on GitHub.
-    cached : bool
+    cached: bool, optional
         Specified if client wants to take repository names from names.txt.
 
     Returns
     -------
-    names : List
+    names: list or None
         List of repository names that end with '-feedstock' (stripped).
+        None if no organization or username is specified.
     '''
     if cached:
         logger.info("reading names")
         with open("names.txt", "r") as f:
             names = f.read().split()
         return names
-
+    if organization == '':
+        logger.info("No GitHub organization specified.")
+        return None
     names = get_all_feedstocks_from_github(organization)
     return names
 
