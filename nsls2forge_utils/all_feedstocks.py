4d21f0eb155fed377a7b0dc9bc8d1a3047f66547
Thomas Hopkins <thomashopkins000@gmail.com>
2020-06-24 09:40:37 -0400

    added netrc for default authentication and moved kwargs to wrapper function

--- nsls2forge_utils/all_feedstocks.py
+++ nsls2forge_utils/all_feedstocks.py
@@ -3,15 +3,16 @@ This code is a rework of
 https://github.com/regro/cf-scripts/blob/master/conda_forge_tick/all_feedstocks.py
 for use by nsls-ii-forge's own auto-tick bot.
 '''
-import github3
-import os
 import datetime
 import logging
+import netrc
+
+import github3
 
 logger = logging.getLogger(__name__)
 
 
-def get_all_feedstocks_from_github(organization, **kwargs):
+def get_all_feedstocks_from_github(organization, username=None, token=None):
     '''
     Gets all public feedstock repository names from the GitHub organization
     (e.g. nsls-ii-forge).
@@ -32,15 +33,10 @@ def get_all_feedstocks_from_github(organization, **kwargs):
     names: list
         List of repository names that end with '-feedstock' (stripped).
     '''
-    if 'username' not in kwargs:
-        username = os.environ['USERNAME']
-    else:
-        username = kwargs['username']
-    if 'password' not in kwargs:
-        password = os.environ['PASSWORD']
-    else:
-        password = kwargs['password']
-    gh = github3.login(username, password)
+    if username is None:
+        netrc_file = netrc.netrc()
+        username, _, token = netrc_file.hosts['github.com']
+    gh = github3.login(username, token)
     org = gh.organization(organization)
     repos = org.repositories()
     names = []
@@ -65,42 +61,45 @@ def get_all_feedstocks_from_github(organization, **kwargs):
     return names
 
 
-def get_all_feedstocks(organization, username=None, password=None, cached=False):
+def get_all_feedstocks(organization=None, cached=False, **kwargs):
     '''
     Gets all feedstocks either from GitHub or from names.txt if flag is specified.
 
     Parameters
     ----------
-    organization: str
-        Name of organization on GitHub.
-    username: str, optional
-        Name of user on GitHub for authentication.
-        Uses environment variable if not specified.
-    password: str, optional
-        Password of user on GitHub for authentication.
-        Uses environment variable if username not specified.
+    organization: str, optional
+        Name of organization on GitHub. Optional only if cached = True.
     cached: bool, optional
         Specified if client wants to take repository names from names.txt.
+    kwargs: dict, optional
+        Username and token may be specified here for authentication.
 
     Returns
     -------
     names: list or None
         List of repository names that end with '-feedstock' (stripped).
-        None if no organization or username is specified.
+        None if no organization or username is specified and cached = False.
     '''
     if cached:
         logger.info("reading names")
         with open("names.txt", "r") as f:
             names = f.read().split()
         return names
-    if organization == '':
+    if organization is None:
         logger.info("No GitHub organization specified.")
         return None
-    names = get_all_feedstocks_from_github(organization, username=username, password=password)
+    if 'username' in kwargs and 'token' in kwargs:
+        username = kwargs['username']
+        token = kwargs['token']
+    else:
+        username = None
+        token = None
+    names = get_all_feedstocks_from_github(organization, username=username, token=token)
     return names
 
 
 def main(args=None):
+    # TODO: move organization to global CONFIG file
     organization = 'nsls-ii-forge'
     names = get_all_feedstocks(organization, cached=False)
     # write each repository name to a file
