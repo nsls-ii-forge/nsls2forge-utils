18445c86485dba059025b99eb35ffa63f5234401
Thomas Hopkins <thomashopkins000@gmail.com>
2020-06-29 14:57:26 -0400

    added basic tests (not working)

--- nsls2forge_utils/tests/test_check_results.py
+++ nsls2forge_utils/tests/test_check_results.py
@@ -1,3 +1,28 @@
+import subprocess
+
 def test_imports():
     from nsls2forge_utils.check_results import (check_conda_channels,  # noqa
                                                 check_package_version)  # noqa
+
+
+def test_default_channel():
+	msg = 'No packages were installed from conda-forge.\n'
+	command = 'conda create --name df && conda activate df && check-results -t channels'
+	res = subprocess.run(command, shell=True, text=True)
+	assert res.returncode == 0
+
+
+def test_forbidden_channel():
+	command = ('conda init --all && conda create --name fc '
+			  '&& conda config --env --add channels conda-forge '
+			  '&& conda activate fc && check-results -t channels')
+	res = subprocess.run(command, shell=True, text=True)
+	assert res.returncode == 1
+
+
+def test_forbidden_channel_ignore():
+	command = ('conda init --all && conda create --name fc '
+			  '&& conda config --env --add channels conda-forge '
+			  '&& conda activate fc && check-results -t channels -i')
+	res = subprocess.run(command, shell=True, text=True)
+	assert res.returncode == 0
