caae6afe9abb38e0d72db3c5a0395ac9762c9ba2
Thomas Hopkins <thomashopkins000@gmail.com>
2020-06-30 15:35:53 -0400

    added test script to manifest/setup and made small changes

--- nsls2forge_utils/tests/test_check_results.sh
+++ nsls2forge_utils/tests/test_check_results.sh
@@ -6,28 +6,45 @@ chmod +x miniconda.sh
 ./miniconda.sh -b -p ~/mc
 source ~/mc/etc/profile.d/conda.sh
 
-# test for defaults channel
-conda create --name df
+# test for default behavior (no packages installed)
+conda create --name df -y
 conda activate df
+conda install -c defaults pip -y
+pip install .
 check-results -t channels
-if [ $? != 0 ]; then
-  echo "ERROR: default channel test failed"
+EXIT="$?"
+if [ $EXIT -ne "0" ]; then
+  echo "ERROR: default behavior test failed with exit code $EXIT"
   exit 1
 fi
 
-# tests for conda-forge channel + ignore
+# test for default channel behavior
+conda install -c defaults python
+check-results -t channels -f conda-forge
+EXIT="$?"
+if [ $EXIT -ne "0" ]; then
+  echo "ERROR: default channel test failed with exit code $EXIT"
+  exit 1
+fi
 conda deactivate
-conda create --name cf
+
+# tests for conda-forge channel + ignore
+conda create --name cf -y
 conda activate cf
-conda config --env --add channels conda-forge
-conda install -c conda-forge --override-channels pip
-check-results -t channels
-if [ $? == 0 ]; then
-  echo "ERROR: forbidden channel raise error test failed"
+FORBIDDEN_NAME="conda-forge"
+conda install -c $FORBIDDEN_NAME --override-channels pip -y
+pip install .
+check-results -t channels -f $FORBIDDEN_NAME
+EXIT="$?"
+if [ $EXIT -eq "0" ]; then
+  echo "ERROR: forbidden channel $FORBIDDEN_NAME raise error test failed with exit code $EXIT"
   exit 1
 fi
-check-results -t channels -i
-if [ $? != 0 ]; then
-  echo "ERROR: ignore forbidden channel test failed"
+check-results -t channels -f $FORBIDDEN_NAME -i
+EXIT="$?"
+if [ $EXIT -ne "0" ]; then
+  echo "ERROR: ignore forbidden channel $FORBIDDEN_NAME test failed with exit code $EXIT"
   exit 1
 fi
+
+echo "SUCCESS: ALL TESTS PASSED"
