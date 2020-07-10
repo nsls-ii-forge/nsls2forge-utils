506374c524822e48d50a962c0f8008e0888cad15
Thomas Hopkins <thomashopkins000@gmail.com>
2020-07-10 15:46:33 -0400

    added pypi and changed table structure

--- nsls2forge_utils/dashboard.py
+++ nsls2forge_utils/dashboard.py
@@ -25,26 +25,26 @@ def create_dashboard(names=None, write_to='README.md'):
     # TODO: Azure Pipeline direct pipeline link
     # TODO: Add codecov badge if available
     main_format = dict(
-      build='[![Build Status](https://dev.azure.com/nsls2forge/nsls2forge/_apis/build/status/{name}-feedstock)]'
+      build='[![Not Found](https://dev.azure.com/nsls2forge/nsls2forge/_apis/build/status/{name}-feedstock)]'
             '(https://dev.azure.com/nsls2forge/nsls2forge/_build)',
-      health='[![Code Health](https://landscape.io/github/nsls-ii-forge/{name}-feedstock/master/'
+      health='[![Not Found](https://landscape.io/github/nsls-ii-forge/{name}-feedstock/master/'
              'landscape.svg?style=flat)](https://landscape.io/github/nsls-ii-forge/{name}-feedstock/master)',
-      cf_version='[![Anaconda-Server Badge](https://anaconda.org/conda-forge/{name}/badges/version.svg)]'
+      cf_version='[![Not Found](https://anaconda.org/conda-forge/{name}/badges/version.svg)]'
                  '(https://anaconda.org/conda-forge/{name})',
-      nsls_version='[![Anaconda-Server Badge](https://anaconda.org/nsls2forge/{name}/badges/version.svg)]'
+      nsls_version='[![Not Found](https://anaconda.org/nsls2forge/{name}/badges/version.svg)]'
                    '(https://anaconda.org/nsls2forge/{name})',
-      downloads='[![Anaconda-Server Badge](https://anaconda.org/nsls2forge/{name}/badges/downloads.svg)]'
+      defaults_version='[![Not Found](https://anaconda.org/anaconda/{name}/badges/version.svg)]'
+                       '(https://anaconda.org/anaconda/{name})',
+      pypi_version='[![Not Found](https://img.shields.io/pypi/v/{name})](https://pypi.org/project/{name}/)',
+      downloads='[![Not Found](https://anaconda.org/nsls2forge/{name}/badges/downloads.svg)]'
                 '(https://anaconda.org/nsls2forge/{name})')
 
-    row_string = '''|[{name}](https://github.com/nsls-ii-forge/{name}-feedstock)|{build}|{health}
-    |{cf_version} <br/> {nsls_version}|{downloads}|\n'''
-    description = '''# Project Management
-    Releases, Installers, Specs, and more!
-    '''
-    header = '''# Feedstock Packages Build status\n
-    | Repo | Build | Health | CF Version <br/> NSLS Version | Downloads|
-    |:-------:|:-----:|:------:|:------:|:------:|
-    '''
+    row_string = ('|[{name}](https://github.com/nsls-ii-forge/{name}-feedstock)|{build} <br/> {health}'
+    '|{nsls_version} <br/> {pypi_version} <br/> {defaults_version} <br/> {cf_version}|{downloads}|\n')
+    description = '''# Project Management\nReleases, Installers, Specs, and more!\n'''
+    header = ('# Feedstock Packages Build Status\n\n'
+    '| Repo | Build <br/> Health | nsls2forge <br/> PyPI <br/> defaults <br/> conda-forge <br/> Versions | Downloads|\n'
+    '|:-------:|:-----------:|:---------------:|:--------------:|\n')
 
     out = description
     if names is None:
